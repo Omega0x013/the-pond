f4719016726eca08995a1a09eb35829792905c2f
Alexander Bielby <xanderbielby+git@gmail.com>
2022-09-30 16:16:52 +0100

    edit index.js

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 import express from 'express';
 import flat from '@omega0x013/flat-express';
 
+// http://boutglay.com/locallydb/
+
+// Initialise server and constants
 const app = express();
 const PORT = 8000;
 
+// Describes route mappings
 const router = flat({
   /* put route mappings in here */
 });
 
+// Set up route listening.
 app.use(router);
 app.use(express.static("src/www"));
 
+// Allow incoming JSON body payloads to be processed.
 app.use(express.json());
 
+// Set the server to await and serve connections.
 app.listen(PORT, () => {
   console.log(`Listening on port ${8000}`);
 });
