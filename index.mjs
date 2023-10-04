6a8156498a6d3b0651acb624e1ac385fe54637fb
Xander Bielby <xanderbielby@gmail.com>
2023-10-04 14:53:11 +0000

    move 'www' to 'docs' for gh pages

--- index.mjs
+++ index.mjs
@@ -2,6 +2,6 @@ import express from 'express';
 
 const PORT = process.env.PORT ?? 8080;
 const app = express();
-app.use(express.static("www"));
+app.use(express.static("docs"));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
