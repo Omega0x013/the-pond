198aeb8fd71d893f73824a7a943beb4a1248ab53
Xander Bielby <xanderbielby@gmail.com>
2024-05-18 18:24:06 +0000

    restructure to remove node

--- index.mjs
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const PORT = process.env.PORT ?? 8080;
-const app = express();
-app.use(express.static("docs"));
-
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
